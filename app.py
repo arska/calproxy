64051839fc3ba746c48b409407f5b44ac646ba5e
Aarno Aukia <aarno.aukia@vshn.ch>
2017-12-21 14:18:28 +0200

    dont start a new thread if there is one already

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, abort
 import os
 import requests
 from werkzeug.contrib.cache import FileSystemCache
 cache = FileSystemCache(cache_dir="cache/")
 import time
 from prometheus_client import Histogram, Counter, Summary, Gauge, REGISTRY, generate_latest
-from threading import Thread
+import threading
 
 app = Flask(__name__)
 FLASK_REQUEST_LATENCY = Histogram('flask_request_latency_seconds', 'Flask Request Latency', ['method', 'endpoint'])
@@ -36,11 +36,13 @@ def calproxy(path):
     # if no auth is required or if proper credentials were provided continue
     data = cache.get(url)
     if data == None:
-        data = update(url)
+        #data = update(url)
+        async_update(url)
+        abort(404)
     age = time.time() - data['time']
     print('data from %d (age %d)' % (data['time'], age))
     if age > os.environ.get('cachetime', 15*60) or age < 0:
-        Thread(target=update,args=(url,)).start()
+        async_update(url)
     resp = Response(data['data'].text)
     resp.headers['Content-Type'] = data['data'].headers['Content-Type']
     return resp
@@ -51,6 +53,13 @@ def update(url):
     data = {'data': r, 'time': time.time()}
     cache.set(url, data, timeout=0)
     return data
+
+def async_update(url):
+    existingthread = False
+    for t in threading.enumerate():
+        if t.name == url:
+            return False
+    threading.Thread(target=update,args=(url,),name=url).start()
 
 def before_request():
     request.start_time = time.time()
